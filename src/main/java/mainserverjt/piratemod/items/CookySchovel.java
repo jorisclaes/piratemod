0d6a82b33fcf729fea0a486c09d1c06850640377
jorisclaes <saxefoon@hotmail.com>
2016-03-19 18:11:28 +0100

    fout gevonden in damag take van pikaxe
    setMaxdamag(int); zegt hoeveel damage het obj kan nemen

--- src/main/java/mainserverjt/piratemod/items/CookySchovel.java
+++ src/main/java/mainserverjt/piratemod/items/CookySchovel.java
@@ -1,6 +1,5 @@
 package mainserverjt.piratemod.items;
 
-import com.google.common.util.concurrent.SettableFuture;
 
 import cpw.mods.fml.common.registry.GameRegistry;
 import mainserverjt.piratemod.PiratemodMain;
@@ -15,25 +14,25 @@ import net.minecraftforge.common.util.EnumHelper;
 public class CookySchovel extends ItemPickaxe {
 	
 	//dit voodbeeld is op :)
-	private static final String name = "CookySchovel";
+	private static  String name = "CookyPickAxe";
 	private static int harvestLevel = 4; //dit zecht wat je kan harvesten
-	private static int maxUses = 2000;//hoe hoger hoelanger hij megaat saat voor blok gemied of mob gehit
-	private static float efficiency = 20.0F;//zegdt hoeveel hij blockt
-	private static float damage = 5.0F;//ze de damage dat die doet
-	private static int enchantability = 1;//zet iest wat bu
-	private static final Item.ToolMaterial mat = EnumHelper.addToolMaterial(name, harvestLevel, maxUses, efficiency, damage, enchantability);
+	private static int maxUses = 3000;//hoe hoger hoelanger hij megaat saat voor blok gemied of mob gehit
+	private static float efficiency = 20.0F;//zegdt hoe geot hij blocks mined
+	private static float damage = 10.0F;//ze de damage dat die doet
+	private static int enchantability = 30;//zet iest wat bu
+	private static Item.ToolMaterial mat = EnumHelper.addToolMaterial(name, harvestLevel, maxUses, efficiency, damage, enchantability);
 	
 	public CookySchovel() {
 		super(mat);
 		setUnlocalizedName(name);
-		GameRegistry.registerItem(this, name);
-		//setTextureName(PiratemodMain.modID + ":" + name);
+		setTextureName(PiratemodMain.modID + ":" + name);
 		setCreativeTab(PiratemodMain.tab);
+		GameRegistry.registerItem(this, this.name);
 		setFull3D();//doe dit weg en dan lijkt het niet alsof je een zwaart vast hebt
 		setNoRepair();//laat je het item niet repairen
 		setContainerItem(PiratemodMain.coocie);//zorg ervoor dat je dit terug krijcht als je iet kraft
 		setMaxStackSize(20);//zet de max tack size dheu
-		setMaxDamage(50);//zet 
+		setMaxDamage(3000);//de max damad dat die kan nemen == maxUses denk ik
 		//setPotionEffect(PotionHelper.sugarEffect);//lees cut
 	}
 }
