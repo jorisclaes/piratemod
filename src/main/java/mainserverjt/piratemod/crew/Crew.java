4c7976bf34d2c42154254fe21f1a15c4e14a0c5d
tomclaes3 <dierenvriend_tomclaes@hotmail.com>
2016-03-27 18:09:22 +0200

    dit keer met source filles (stopid)

---
+++ src/main/java/mainserverjt/piratemod/crew/Crew.java
@@ -0,0 +1,88 @@
+package mainserverjt.piratemod.crew;
+
+import java.util.HashMap;
+
+import mainserverjt.piratemod.Main;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayer;
+
+public class Crew {
+
+	private EntityPlayer[] personen;
+	private int id;
+	private Main main;
+	private int rank;
+	private double gemiddeldeXpLvl;
+	private int funding;
+	
+	
+	
+	//private Ship currentship;
+	//private Weapon currentWeaponRank;
+	/**
+	 * maakt een crew aan
+	 * @param main main variable 
+	 * @param crewid id van crew
+	 * @param personen de lijst van personen behoordende tot de crew
+	 */
+	public Crew(Main main,int crewid, EntityPlayer[] personen){
+		this.main = main;
+		id = crewid;
+		this.personen = personen;
+		rank = 0;
+		gemiddeldeXpLvl = 0;
+		funding = 0;
+		
+	}
+	
+	//setter
+	/**
+	 * set de rank
+	 * @param rank int rank
+	 */
+	public void setRank(int rank){
+		this.rank = rank;
+	}
+	
+	
+	/**
+	 * returnt een rank
+	 * @return rank as int
+	 */
+	//getter
+	public int getRank(){
+		return rank;
+	}
+	
+	/**
+	 * returnt een gemiddelde xp lvl
+	 * @return double van gxl
+	 */
+	public double getGemiddeldeXpLvl() {
+		return gemiddeldeXpLvl;
+	}
+
+	/**
+	 * zet de variable als gxl
+	 * @param gemiddeldeXpLvl
+	 */
+	public void setGemiddeldeXpLvl(double gemiddeldeXpLvl) {
+		this.gemiddeldeXpLvl = gemiddeldeXpLvl;
+	}
+
+	/**
+	 * return funding word gebruikt voor upgreads te kopen (weapon en ship)
+	 * @return funding las intger
+	 */
+	public int getFunding() {
+		return funding;
+	}
+
+	/**
+	 * set de funding
+	 * @param funding int
+	 */
+	public void setFunding(int funding) {
+		this.funding = funding;
+	}
+}
