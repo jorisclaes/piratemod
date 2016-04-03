05907122cad5574adb2557addefa2826f4f404e9
tomclaes3 <dierenvriend_tomclaes@hotmail.com>
2016-04-04 01:59:29 +0200

    lvl xp berekening

---
+++ src/main/java/test/Test.java
@@ -0,0 +1,19 @@
+package test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double x= 100;
+		int y = 2;
+		for(int i = 0; i < 100; i++){
+			double f =(y/x)*((y+x)*y*55.55555) ;
+			f = (int) (f * 100+ 0.5) / 100;
+			System.out.println("lvl: " + (i+1) + " -> "+ f);
+			x = f;
+			y += 1;
+		}
+
+	}
+
+}
